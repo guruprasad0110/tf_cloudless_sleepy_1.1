6360ba2387400bb5ccf7a6b508179d42563c6b8f
Guru Prasad Shamanna <34366029+guruprasad0110@users.noreply.github.com>
2022-11-30 14:19:36 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,11 +2,9 @@
 terraform {
   required_version = ">= 1.0.0"
   required_providers {
-    null = {
-      source = "hashicorp/null"
-    }
-    template = {
-      source = "hashicorp/template"
-    }
-  }
+		ibm1 =  {
+			source = "jonwoodlief/ibm"
+			version = "4.1.0"
+		}
+	}
 }
